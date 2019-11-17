df555cbfde55e304848fda04f05de0a691acb200
Teddy Leung <teddy.ph.leung@gmail.com>
2019-11-17 09:32:32 -0800

    Added unique constraint to users_username and created games_uuid and index.

--- database/migrations/1_schema.sql
+++ database/migrations/1_schema.sql
@@ -3,9 +3,11 @@ DROP TABLE IF EXISTS game_types CASCADE;
 DROP TABLE IF EXISTS games CASCADE;
 DROP TABLE IF EXISTS user_games CASCADE;
 
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  username VARCHAR(255) NOT NULL,
+  username VARCHAR(255) UNIQUE NOT NULL,
   password VARCHAR(255) NOT NULL
 );
 
@@ -18,6 +20,7 @@ CREATE TABLE game_types (
 
 CREATE TABLE games (
   id SERIAL PRIMARY KEY NOT NULL,
+  uuid UUID DEFAULT uuid_generate_v4(),
   game_type_id INTEGER REFERENCES game_types(id) ON DELETE CASCADE,
   creator_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   created_at TIMESTAMP DEFAULT NOW(),
@@ -26,6 +29,9 @@ CREATE TABLE games (
   deleted_at TIMESTAMP,
   game_state JSONB
 );
+
+CREATE INDEX games_uuid_index 
+  ON games(uuid);
 
 CREATE TABLE user_games (
   id SERIAL PRIMARY KEY NOT NULL,
