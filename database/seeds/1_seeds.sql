cea231bea4a3399daf54d05ac0f4e58799b6face
denven <myregs@foxmail.com>
2019-11-18 19:17:21 -0800

    seed without game_types

--- database/seeds/1_seeds.sql
+++ database/seeds/1_seeds.sql
@@ -4,25 +4,18 @@ INSERT INTO users (username, password)
     ('Chris', 'password'),
     ('David', 'password');
 
-INSERT INTO games (game_type_id, creator_id, game_state)
-  VALUES (1, 1, '{"players": {"Alice": {"cards": [1,2,3,4,5,6,7,8,9,10,11,12,13]}, "Bob": {"cards": [1,2,3,4,5,6,7,8,9,10,11,12,13]}}, "history": [{"Alice": null, "Bob": null}], "cards": [13, 8, 5, 7, 4, 12, 10, 1, 3, 9, 2, 6, 11]}'),
-    (1, 2, null),
-    (1, 3, null),
-    (1, 4, null),
-    (2, 1, null),
-    (2, 2, null),
-    (2, 3, null),
-    (2, 4, null);
+INSERT INTO games (game_type_id, creator_id, created_at, started_at, completed_at,  deleted_at,  game_state)
+   VALUES 
+(1, 1, '2019-11-15 15:30:15', '2019-11-15 15:30:20', '2019-11-15 15:40:20', NULL, NULL),
+(1, 2, '2019-11-15 16:30:15', '2019-11-15 16:30:20', '2019-11-15 16:45:20', NULL, NULL),
+(1, 1, '2019-11-15 16:40:15', '2019-11-15 16:45:20', NULL, NULL, NULL),
+(2, 2, '2019-11-16 17:40:15', '2019-11-16 17:40:15', NULL, NULL, NULL),
+(2, 3, '2019-11-16 17:50:25', NULL, NULL, NULL, NULL),
+(2, 4, '2019-11-16 17:54:15', NULL, NULL, NULL, NULL),
+(1, 1, '2019-11-16 18:40:15', NULL, NULL, '2019-11-16 18:40:15', NULL),
+(1, 1, '2019-11-16 18:40:25', NULL, NULL, '2019-11-16 18:40:15', NULL);
 
-INSERT INTO user_games (user_id, game_id)
-  VALUES (1, 1),
-    (2, 1),
-    (2, 2),
-    (3, 3),
-    (4, 4),
-    (1, 5),
-    (2, 5),
-    (3, 5),
-    (2, 6),
-    (3, 7),
-    (4, 8);
+INSERT INTO user_games ( user_id, game_id)
+	VALUES (1, 1), 	(2,1), (2, 2), (3, 2), (1, 3), (2, 3), (2, 4), (1, 4), (3, 4), (3, 5), (4, 6),
+               (1,7),  (2,7),  (1,8);
+
