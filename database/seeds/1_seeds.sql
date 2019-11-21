2cf6638259f375da2ed64a5d43da5c58b2ece92e
denven <myregs@foxmail.com>
2019-11-21 15:43:29 -0800

    deleted duplicated rows'

--- database/seeds/1_seeds.sql
+++ database/seeds/1_seeds.sql
@@ -11,12 +11,6 @@ INSERT INTO game_types (name, file_name, player_min, player_max)
   VALUES ('Goofspiel', 'goofspiel', 2, 2),
     ('Cards Against Everything', 'cards_against_everything', 3, 6);
 
-INSERT INTO games (game_type_id, creator_id, created_at, started_at, completed_at,  deleted_at,  game_state)
-   VALUES (1, 1, '2019-11-15 15:30:15', '2019-11-15 15:30:20', '2019-11-15 15:40:20', NULL, NULL),
-	(1, 2, '2019-11-15 16:30:15', '2019-11-15 16:30:20', '2019-11-15 16:45:20', NULL, NULL),
-	(1, 1, '2019-11-15 16:40:15', '2019-11-15 16:45:20', NULL, NULL, NULL),
-	(2, 2, '2019-11-15 17:40:15', NULL, NULL, NULL, NULL);
-
 INSERT INTO user_games (user_id, game_id)
 	VALUES (1, 1), 	(2,1), (1, 2), (3, 2), (3, 3), (2, 3), (1, 4);
 
