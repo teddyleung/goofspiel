45c076d53d310356c8fec5f8eadec58a340e14ce
denven <myregs@foxmail.com>
2019-11-21 21:23:36 -0800

    leaderboard and archives

--- src/server/routes/routes.js
+++ src/server/routes/routes.js
@@ -148,8 +148,8 @@ module.exports = function(router, db) {
             ...game,
             completed_at: helpers.formatGameDate(game.completed_at)
           }));
-          console.log(newCompletedGames);
-          res.render(`archives`, {"accountName": username, "completedGames": newCompletedGames});
+          let gameTypes = helpers.getAllGamesName(newCompletedGames, 'name');
+          res.render(`archives`, {"accountName": username, "gameTypes": gameTypes,"completedGames": newCompletedGames});
           return;
         })
         .catch(e => res.send(e));
